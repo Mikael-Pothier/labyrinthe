1a1f8df2ee4c58a837e7e4c346bc9baa19562b9c
chunksahoy <charles_hunter182@hotmail.com>
2015-03-21 13:03:43 -0400

    class Commande

---
+++ labyrinthe/Commande.h
@@ -0,0 +1,33 @@
+#pragma once
+
+class CCommande
+{
+	char symbole_;
+public:
+	
+	CCommande(char symbole) throw() 
+		: symbole_(symbole)
+	{
+
+	}
+	char GetSymbole() const throw()
+	{
+		return symbole_;
+	}
+	bool opertor==(const CCommande &c)
+		const throw()
+	{
+		return GetSymbole() == c.GetSymbole();
+	}
+	bool operator!=(const CCommande &c)
+		const throw()
+	{
+		return !(this == c);
+	}
+};
+
+#include <iosfwd>
+using std::istream;
+using std::ostream;
+ostream & operator<<(ostream&, const CCommande &);
+istream & operator>>(istream&, CCommande &);
